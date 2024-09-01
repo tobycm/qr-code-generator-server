7c74d3a8d1cbc6d685bcf23571fa35f2af174ead
Toby Cm <toby@vsus.app>
2024-08-31 18:40:26 -0700

    way more options for the qr endpoint :D

--- README.md
+++ README.md
@@ -1,15 +1,59 @@
-# qr-code-generator-server
+# QR Code Generator Server
 
-To install dependencies:
+A simple API server for generating QR codes.
 
-```bash
-bun install
+## 💪 Features
+
+- ✅ Asynchronous QR code generation
+- ✅ Fast and lightweight
+- ✅ Different formats (PNG, SVG, etc.)
+- ✅ Customizable QR code size, color, etc
+
+## 🚀 Getting Started
+
+### Prerequisites
+
+- [Bun](https://bun.sh)
+
+### Step 1
+
+Clone the repository
+
+```sh {"id":"01J6K7HS08C87M42HE07X7GXAY"}
+git clone https://github.com/tobycm/qr-code-generator-server
+cd qr-code-generator-server
 ```
 
-To run:
+### Step 2
 
-```bash
-bun run index.ts
+Install the dependencies
+
+```sh {"id":"01J6K7JKVY7RS3486R0TW8XXAG"}
+bun i
 ```
 
-This project was created using `bun init` in bun v1.1.20. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+### Step 3
+
+Start the server
+
+```sh {"id":"01J6K7P605DHZ0FABRMZHVTPZ3"}
+bun run start
+```
+
+## 📚 Tech Stack
+
+- [Bun](https://bun.sh)
+- [node-qrcode](https://www.npmjs.com/package/qrcode)
+- [TypeScript](https://www.typescriptlang.org)
+
+## 📝 License
+
+This project is licensed under the MIT License - see the [`LICENSE`](LICENSE) file for details.
+
+## 🤝 Contributions
+
+Any contribution is appreciated. Just create an issue/pull request and I will review it ASAP.
+
+## 🔗 Share this project
+
+If you like this project, please give it a ⭐ and share it with your friends!
--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
+import { existsSync, mkdirSync } from "fs";
+import { tmpdir } from "os";
 import * as QR from "qrcode";
+
+const tempFolder = tmpdir() + "/qr-code-generator-server/";
+if (!existsSync(tempFolder)) mkdirSync(tempFolder);
 
 Bun.serve({
   async fetch(request) {
@@ -16,16 +21,82 @@ Bun.serve({
       });
     }
 
-    let errorCorrection = url.searchParams.get("errorCorrection") ?? "";
-    if (!["L", "M", "Q", "H"].includes(errorCorrection)) errorCorrection = "M";
+    if (url.pathname !== "/qr") {
+      return new Response("Not found", {
+        status: 404,
+      });
+    }
+
+    const format = url.searchParams.get("format") ?? "png";
+    if (!["png", "svg", "utf8"].includes(format)) {
+      return new Response("Invalid format", {
+        status: 400,
+      });
+    }
+
+    const margin = url.searchParams.get("margin") ?? "4";
+    if (!margin || isNaN(parseInt(margin))) {
+      return new Response("Invalid margin", {
+        status: 400,
+      });
+    }
+
+    const errorCorrection = url.searchParams.get("errorCorrection") ?? undefined;
+    if (![undefined, "L", "M", "Q", "H"].includes(errorCorrection)) {
+      return new Response("Invalid error correction", {
+        status: 400,
+      });
+    }
+
+    const scale = url.searchParams.get("scale") ?? "4";
+    if (!scale || isNaN(parseInt(scale))) {
+      return new Response("Invalid scale", {
+        status: 400,
+      });
+    }
+
+    const width = url.searchParams.get("width") ?? undefined;
+    if (width && isNaN(parseInt(width))) {
+      return new Response("Invalid width", {
+        status: 400,
+      });
+    }
+
+    const background = url.searchParams.get("background") ?? undefined;
+    if (background && !/^#[0-9A-F]{8}$/i.test(background)) {
+      return new Response("Invalid background", {
+        status: 400,
+      });
+    }
+
+    const foreground = url.searchParams.get("foreground") ?? undefined;
+    if (foreground && !/^#[0-9A-F]{8}$/i.test(foreground)) {
+      return new Response("Invalid foreground", {
+        status: 400,
+      });
+    }
 
     const body = await request.text();
 
-    const image = await QR.toBuffer(body, { errorCorrectionLevel: errorCorrection as "L" | "M" | "Q" | "H" });
+    const imageFile = `${tempFolder}${Date.now()}.${format}`;
 
-    return new Response(image, {
+    await QR.toFile(imageFile, body, {
+      errorCorrectionLevel: errorCorrection as "L" | "M" | "Q" | "H",
+      type: format as "png" | "svg" | "utf8",
+      margin: parseInt(margin),
+      scale: parseInt(scale),
+      width: width ? parseInt(width) : undefined,
+      color: {
+        dark: foreground,
+        light: background,
+      },
+    });
+
+    const file = Bun.file(imageFile);
+
+    return new Response(file, {
       headers: {
-        "content-type": "image/png",
+        "content-type": format === "utf8" ? "text/plain" : `image/${format}`,
       },
     });
   },
